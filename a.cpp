392afbd7aae41c6a94eb33019712b4343cee5118
Md.Moshiur Rahman Sajol <moshiur287rahman@gmail.com>
2025-07-31 13:29:06 +0600

    Add files via upload

---
+++ a.cpp
@@ -0,0 +1,12 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    int n, a, x, i;
+    cin>>n;
+    cin>>x;
+   int A[100];
+    for(i = 1; i < n; i++)
+    cin>>A[i];
+    
+}
---
+++ amp1.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+
+int main() 
+{
+    long long A, B;
+    cin >> A >> B;
+
+    long long result = (A + B - 1) / B;
+
+    cout << result << endl;
+    return 0;
+}
